6f87a43c7d15d7e43e9aa41b59b87ed20619bfeb
Bruna Moncao Estrada <bruna.estrada@matera.com>
2021-09-28 21:07:47 -0300

    Criando usuario e alterando conta corrente

---
+++ src/main/java/matera/bootcamp/pix/domain/model/ContaCorrente.java
@@ -0,0 +1,24 @@
+package matera.bootcamp.pix.domain.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+public class ContaCorrente {
+    //@Id que será usado pelo framework;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long agencia;
+
+    @Column(nullable = false)
+    private Long conta;
+
+    @Column(precision = 10, scale = 2, nullable = false)
+    private BigDecimal saldo;
+}
